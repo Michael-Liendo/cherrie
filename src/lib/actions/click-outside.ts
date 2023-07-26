5b9cb5b74788670c9b89f65d1b101641a1206043
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-26 19:02:27 -0400

    chore: actions

---
+++ src/lib/actions/click-outside.ts
@@ -0,0 +1,24 @@
+/** Dispatch event on click outside of node */
+// rome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function clickOutside(node: HTMLElement): any {
+	const handleClick = (event: MouseEvent) => {
+		if (
+			node &&
+			!node.contains(event.target as Node) &&
+			!event.defaultPrevented
+		) {
+			node.dispatchEvent(new CustomEvent("clickOutside"));
+		}
+	};
+
+	document.addEventListener("click", handleClick, true);
+
+	return {
+		destroy() {
+			document.removeEventListener("click", handleClick, true);
+		},
+		on: {
+			clickOutside: handleClick,
+		},
+	};
+}
