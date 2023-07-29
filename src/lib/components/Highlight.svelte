b6f0876e43e4aa8362c51e65174f84b02515546d
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-29 13:17:17 -0400

    fix: highlight icon

--- src/lib/components/Highlight.svelte
+++ src/lib/components/Highlight.svelte
@@ -12,7 +12,7 @@
     <button
       class="bg-slate-100 border border-slate-200 p-2.5 text-xl flex justify-center items-center rounded-lg"
     >
-      <Icon />
+      <svelte:component this={Icon} />
     </button>
     <div>
       <h1 class="text-lg my-2 font-semibold">{highlight.title}</h1>
