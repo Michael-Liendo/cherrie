5389e213a9ccfb5e77525845e64ddabb988cb548
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-02 21:14:21 -0400

    feat: favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,8 +27,8 @@
   <meta name="copyright" content="Sarteza" />
   <meta name="robots" content="index, follow" />
 
-  <!-- <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" /> -->
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
 </svelte:head>
 
 {#if notification}
