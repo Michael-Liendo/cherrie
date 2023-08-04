a84acce05a9d211b622a9b91a88bcae9290a09d6
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-04 16:34:35 -0400

    feat: product

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -3,6 +3,27 @@
   import ProductCard from '$lib/components/Home/ProductCard.svelte';
 </script>
 
+<svelte:head>
+  <title>Productos - Sarteza</title>
+  <meta name="description" content={'Lista de productos de sarteza'} />
+  <meta
+    name="keywords"
+    content="Sarteza, ropa, accesorios, moda, tendencias, products de sarteza, mejores productos"
+  />
+  <!-- Schema.org markup for Google+ -->
+  <meta itemprop="name" content="Productos - Sarteza" />
+  <meta itemprop="description" content={'Lista de productos de sarteza'} />
+
+  <meta itemprop="image" content="https://sarteza.com/images/logo.png" />
+  <!-- Open Graph data -->
+  <meta property="og:title" content="Productos - Sarteza" />
+  <meta property="og:type" content="product" />
+  <meta property="og:url" content="https://sarteza.com/products" />
+  <meta property="og:image" content="https://sarteza.com/images/logo.png" />
+  <meta property="og:description" content="Lista de productos de sarteza" />
+  <meta property="og:site_name" content="Sarteza" />
+</svelte:head>
+
 <nav class="flex mt-5">
   <ol role="list" class="flex items-center">
     <li class="text-left">
