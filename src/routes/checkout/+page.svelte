4756672d58f034ccd35daae5c0dc3edf25596271
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-05 23:03:32 -0400

    feat: better form

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-  import MdiEmailOutline from '~icons/mdi/email-outline';
+  import { newForm } from '@whizzes/svelte-forms';
+  import * as Yup from 'yup';
   import MdiAccountOutline from '~icons/mdi/account-outline';
 
   import { cart, clearCart } from '$lib/store/cart';
-
-  function completeOrder() {
-    // clearCart();
-  }
 
   let customerName = '';
   let customerPayment = '';
@@ -19,9 +16,30 @@
     )
     .toFixed(2);
 
-  $: linkToOrder = `https://wa.me/584125719288?text=*Nombre:* ${customerName}%0A*Método Pago:* ${customerPayment}%0A *PRODUCTOS:*%0A${$cart
-    .map((item) => `${item.name} - ${item.price} - ${item.quantity}%0A`)
-    .join(', ')}%0A *TOTAL:* $${cartTotal}`;
+  $: cartItemsTotal = $cart.reduce(
+    (accumulator: number, product) => accumulator + product.quantity,
+    0
+  );
+
+  const { handleSubmit, values, errors } = newForm({
+    initialValues: {
+      name: '',
+      payment: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required('Nombre es requerido'),
+      payment: Yup.string().required('Tipo de pago es requerido'),
+    }),
+    onSubmit(values, helpers) {
+      window.location.href = `https://wa.me/584149409930?text=*Nombre:* ${customerName}%0A*Método Pago:* ${customerPayment}%0A*PRODUCTOS:*%0A${$cart
+        .map(
+          (item) =>
+            `Nombre: ${item.name} - $${item.price} - Cantidad: ${item.quantity}%0A - SLUG: ${item.slug}`
+        )
+        .join(', ')}%0A*TOTAL:* $${cartTotal}`;
+      clearCart();
+    },
+  });
 </script>
 
 <svelte:head>
@@ -83,7 +101,10 @@
       {/each}
     </div>
   </div>
-  <div class="rounded-lg h-fit bg-gray-100 px-4 pt-5 my-5 lg:mt-0">
+  <form
+    on:submit={handleSubmit}
+    class="rounded-lg h-fit bg-gray-100 px-4 pt-5 my-5 lg:mt-0"
+  >
     <p class="text-xl font-medium">Detalles de compra</p>
     <p class="text-gray-400">Complete su pedido facilitando sus datos.</p>
     <div>
@@ -97,7 +118,7 @@
           name="card-holder"
           class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
           placeholder="John Doe"
-          bind:value={customerName}
+          bind:value={$values.name}
         />
         <div
           class="pointer-events-none text-gray-400 absolute inset-y-0 left-0 inline-flex items-center px-3"
@@ -105,6 +126,9 @@
           <MdiAccountOutline />
         </div>
       </div>
+      {#if $errors.name}
+        <p class="text-sm font-medium text-red-600">{$errors.name}</p>
+      {/if}
 
       <label
         for="countries"
@@ -112,15 +136,19 @@
         >Selecciona un método de pago</label
       >
       <select
-        bind:value={customerPayment}
-        id="countries"
+        bind:value={$values.payment}
         class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
       >
-        <option selected>Selecciona un método de pago</option>
+        <option value="" selected disabled hidden
+          >Selecciona un método de pago</option
+        >
         <option value="pago-movil">Pago Móvil</option>
         <option value="transferencia">Transferencia bancaria</option>
         <option value="zinli">Zinli</option>
       </select>
+      {#if $errors.payment}
+        <p class="text-sm font-medium text-red-600">{$errors.payment}</p>
+      {/if}
 
       <!-- <label for="email" class="mt-4 mb-2 block text-sm font-medium">
         Email
@@ -140,7 +168,13 @@
         </div>
       </div> -->
 
-      <div class="mt-6 border-t border-b py-2">
+      <div class="mt-6 border-t py-2">
+        <div class="flex items-center justify-between">
+          <p class="text-sm font-medium text-gray-900">Cantidad de productos</p>
+          <p class="font-semibold text-gray-900">{cartItemsTotal}</p>
+        </div>
+      </div>
+      <div class="border-t border-b py-2">
         <div class="flex items-center justify-between">
           <p class="text-sm font-medium text-gray-900">Subtotal</p>
           <p class="font-semibold text-gray-900">${cartTotal}</p>
@@ -151,13 +185,11 @@
         <p class="text-2xl font-semibold text-gray-900">${cartTotal}</p>
       </div>
     </div>
-    <a href={linkToOrder}>
-      <button
-        on:click={completeOrder}
-        class="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white"
-      >
-        Realizar pedido
-      </button>
-    </a>
-  </div>
+    <button
+      type="submit"
+      class="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white"
+    >
+      Realizar pedido
+    </button>
+  </form>
 </div>
